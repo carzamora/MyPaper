20f313637feb2f2ba70ec4e1204ea88a9069a57e
t4x0n <carzamora@gmail.com>
2022-08-12 16:00:46 -0400

    Initial commit

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2022 t4x0n
+Copyright (c) 2022 Carlos Zamora Manzur
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ config.R
@@ -0,0 +1,28 @@
+library(usethis)
+
+# configure git
+use_git_config(user.name = "mat1506",
+               user.email = "matutefrugone@gmail.com")
+# intialise git and commit
+usethis::use_git()
+
+usethis::use_mit_license("Carlos Zamora Manzur")
+
+rrtools::use_readme_rmd()
+
+# Crear repositorio de GitHub
+# no necesario
+usethis::edit_r_environ()
+# esto es para crear un token (no necesario)
+usethis::create_github_token()
+
+
+# configuré GitHub PAT credentials
+gitcreds::gitcreds_set() # acá se mete el token
+# este es mi token:  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
+# creá un GitHub repository and push
+usethis::use_git()
+#hacer un commit
+usethis::use_github()
+#en el terminal git push -u origin master
+
